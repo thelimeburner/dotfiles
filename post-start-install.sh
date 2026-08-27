8e09ce8dd26d7b1c8afdf68f9e447adb01ef8251
Lucas Chaufournier <lucaschaufournier@gmail.com>
2026-08-27 15:44:45 +0000

    updating with defv

--- post-start-install.sh
+++ post-start-install.sh
@@ -3,7 +3,7 @@ set -x
 
 # All of these segments will be symlinked from the home directory to your EFS drive.
 # You can safely customize this list.
-for segment in .claude .claude.json .codex .oh-my-zsh .zshenv .zprofile .zsh_history .zshrc .cursor .cursor-server .taskbuddy task-buddy; do
+for segment in .dev .claude .claude.json .codex .oh-my-zsh .zshenv .zprofile .zsh_history .zshrc .cursor .cursor-server .taskbuddy task-buddy; do
   # If you don't want to delete what's currently in the home directory, remove this.
   if [ -e "$HOME/$segment" ]; then
     rm -rf "$HOME/$segment"
